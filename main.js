beea4ab6557e987a41c7c13a6080ced9194bd5ed
John Doe <johndoe@example.com>
2023-11-11 11:22:19 +0700

    Add sum array functionality

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 function sum(a,b)
 {
     if(Array.isArray(a)){
-        let total= 0
-        for(let i =0; i<a.length; i++){
-            total += a[i]
-        }
-        return total
-
+       
+       return a.reduce((acc,n) => acc + n, 0)
+        
     }
     else if (typeof a === 'number' && typeof b === 'number'){
     return a+b}
